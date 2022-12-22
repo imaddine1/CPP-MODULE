e55a4b0d92456208306ffb715422e90a41f4c137
imaddine1 <hh.imad2015@gmail.com>
2022-12-22 20:35:36 +0100

    M:D1E3

--- Day01/ex03/HumanB.cpp
+++ Day01/ex03/HumanB.cpp
@@ -3,6 +3,7 @@
 HumanB::HumanB(std::string name)
 {
 	this->name = name;
+	this->weapon = NULL;
 }
 
 void	HumanB::setWeapon(Weapon& we)
@@ -12,6 +13,12 @@ void	HumanB::setWeapon(Weapon& we)
 
 void	HumanB::attack()
 {
+	if (!this->weapon)
+	{
+		std::cout << "i don't have a weapon\n";
+		return ;
+	}
 	std::cout << this->name << std::endl;
 	std::cout << "attacks with their " << this->weapon->getType() << std::endl;
+
 }
